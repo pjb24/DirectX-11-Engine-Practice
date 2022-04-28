dd5a7ce1702ba4ba3f4f7d1c6cecd32bca796c19
박재복 <pjb24@daum.net>
2022-04-28 16:13:14 +0900

    마우스 입력 추가

--- DirectX-11-Engine-Practice/Engine.cpp
+++ DirectX-11-Engine-Practice/Engine.cpp
@@ -22,4 +22,9 @@ void Engine::Update()
 		KeyboardEvent kbe = keyboard.ReadKey();
 		unsigned char keycode = kbe.GetKeyCode();
 	}
+
+	while ( !mouse.EventBuffersIsEmpty() )
+	{
+		MouseEvent me = mouse.ReadEvent();
+	}
 }
--- DirectX-11-Engine-Practice/WindowContainer.h
+++ DirectX-11-Engine-Practice/WindowContainer.h
@@ -1,16 +1,15 @@
 #pragma once
 #include "RenderWindow.h"
 #include "Keyboard/KeyboardClass.h"
+#include "Mouse/MouseClass.h"
 
 class WindowContainer
 {
 public:
-	// WindowProc
 	LRESULT WindowProc( HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam );
-	// Mouse
-	// Keyboard
 
 protected:
 	RenderWindow render_window;
 	KeyboardClass keyboard;
+	MouseClass mouse;
 };
