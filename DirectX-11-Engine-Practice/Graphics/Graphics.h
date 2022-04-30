ec85d0a0163aca2751cdb4a2de7e7329e0f3cb13
박재복 <pjb24@daum.net>
2022-04-30 14:22:54 +0900

    InputLayout을 VertexShader 안으로 이동

--- DirectX-11-Engine-Practice/Graphics/Graphics.h
+++ DirectX-11-Engine-Practice/Graphics/Graphics.h
@@ -17,7 +17,5 @@ private:
 	Microsoft::WRL::ComPtr<IDXGISwapChain> swapchain;
 	Microsoft::WRL::ComPtr<ID3D11RenderTargetView> renderTargetView;
 
-	Microsoft::WRL::ComPtr<ID3D11InputLayout> inputLayout;
-
 	VertexShader vertexshader;
 };
