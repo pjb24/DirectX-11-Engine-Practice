8a0f5c43ff7a5d2b7c3a3514821fdb42058ac942
박재복 <pjb24@daum.net>
2022-04-30 16:28:17 +0900

    DepthStencil을 이용해 Z-buffering 구현

--- DirectX-11-Engine-Practice/Graphics/Graphics.cpp
+++ DirectX-11-Engine-Practice/Graphics/Graphics.cpp
@@ -24,18 +24,25 @@ void Graphics::RenderFrame()
 {
 	float bgcolor[] = { 0.0f, 0.0f, 0.0f, 1.0f };
 	this->deviceContext->ClearRenderTargetView( this->renderTargetView.Get(), bgcolor );
+	this->deviceContext->ClearDepthStencilView( this->depthStencilView.Get(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0 );
 
 	this->deviceContext->IASetInputLayout( this->vertexshader.GetInputLayout() );
 	this->deviceContext->IASetPrimitiveTopology( D3D11_PRIMITIVE_TOPOLOGY::D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST );
 	this->deviceContext->RSSetState( this->rasterizerState.Get() );
+	this->deviceContext->OMSetDepthStencilState( this->depthStencilState.Get(), 0 );
 
 	this->deviceContext->VSSetShader( vertexshader.GetShader(), NULL, 0 );
 	this->deviceContext->PSSetShader( pixelshader.GetShader(), NULL, 0 );
 
 	UINT stride = sizeof( Vertex );
 	UINT offset = 0;
-	this->deviceContext->IASetVertexBuffers( 0, 1, vertexBuffer.GetAddressOf(), &stride, &offset );
 
+	// Red Tri
+	this->deviceContext->IASetVertexBuffers( 0, 1, vertexBuffer.GetAddressOf(), &stride, &offset );
+	this->deviceContext->Draw( 3, 0 );
+
+	// Green Tri
+	this->deviceContext->IASetVertexBuffers( 0, 1, vertexBuffer2.GetAddressOf(), &stride, &offset );
 	this->deviceContext->Draw( 3, 0 );
 
 	this->swapchain->Present( 1, NULL );
@@ -107,7 +114,50 @@ bool Graphics::InitializeDirectX( HWND hwnd, int width, int height )
 		return false;
 	}
 
-	this->deviceContext->OMSetRenderTargets( 1, this->renderTargetView.GetAddressOf(), NULL );
+	// Describe Depth/Stencil Buffer
+	D3D11_TEXTURE2D_DESC depthStencilDesc;
+	depthStencilDesc.Width = width;
+	depthStencilDesc.Height = height;
+	depthStencilDesc.MipLevels = 1;
+	depthStencilDesc.ArraySize = 1;
+	depthStencilDesc.Format = DXGI_FORMAT_D24_UNORM_S8_UINT;
+	depthStencilDesc.SampleDesc.Count = 1;
+	depthStencilDesc.SampleDesc.Quality = 0;
+	depthStencilDesc.Usage = D3D11_USAGE_DEFAULT;
+	depthStencilDesc.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	depthStencilDesc.CPUAccessFlags = 0;
+	depthStencilDesc.MiscFlags = 0;
+
+	hr = this->device->CreateTexture2D( &depthStencilDesc, NULL, this->depthStencilBuffer.GetAddressOf() );
+	if ( FAILED( hr ) )	// If error occurred
+	{
+		ErrorLogger::Log( hr, "Failed to create depth stencil buffer." );
+		return false;
+	}
+
+	hr = this->device->CreateDepthStencilView( this->depthStencilBuffer.Get(), NULL, this->depthStencilView.GetAddressOf() );
+	if ( FAILED( hr ) )	// If error occurred
+	{
+		ErrorLogger::Log( hr, "Failed to create depth stencil view." );
+		return false;
+	}
+
+	this->deviceContext->OMSetRenderTargets( 1, this->renderTargetView.GetAddressOf(), this->depthStencilView.Get() );
+
+	// Create depth stencil state
+	D3D11_DEPTH_STENCIL_DESC depthstencildesc;
+	ZeroMemory( &depthstencildesc, sizeof( D3D11_DEPTH_STENCIL_DESC ) );
+
+	depthstencildesc.DepthEnable = true;
+	depthstencildesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK::D3D11_DEPTH_WRITE_MASK_ALL;
+	depthstencildesc.DepthFunc = D3D11_COMPARISON_FUNC::D3D11_COMPARISON_LESS_EQUAL;
+
+	hr = this->device->CreateDepthStencilState( &depthstencildesc, this->depthStencilState.GetAddressOf() );
+	if ( FAILED( hr ) )
+	{
+		ErrorLogger::Log( hr, "Failed to create depth stencil state." );
+		return false;
+	}
 
 	// Create the Viewport
 	D3D11_VIEWPORT viewport;
@@ -117,6 +167,8 @@ bool Graphics::InitializeDirectX( HWND hwnd, int width, int height )
 	viewport.TopLeftY = 0;
 	viewport.Width = width;
 	viewport.Height = height;
+	viewport.MinDepth = 0.0f;
+	viewport.MaxDepth = 1.0f;
 
 	// Set the Viewport
 	this->deviceContext->RSSetViewports( 1, &viewport );
@@ -161,7 +213,7 @@ bool Graphics::InitializeShaders()
 
 	D3D11_INPUT_ELEMENT_DESC layout[] =
 	{
-		{ "POSITION", 0, DXGI_FORMAT::DXGI_FORMAT_R32G32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_CLASSIFICATION::D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "POSITION", 0, DXGI_FORMAT::DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_CLASSIFICATION::D3D11_INPUT_PER_VERTEX_DATA, 0 },
 		{ "COLOR", 0, DXGI_FORMAT::DXGI_FORMAT_R32G32B32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_CLASSIFICATION::D3D11_INPUT_PER_VERTEX_DATA, 0 }
 	};
 
@@ -182,11 +234,12 @@ bool Graphics::InitializeShaders()
 
 bool Graphics::InitializeScene()
 {
+	// Red Triangle
 	Vertex v[] =
 	{
-		Vertex( -0.5f, -0.5f, 1.0f, 0.0f, 0.0f ),	// Left Point
-		Vertex( 0.0f, 0.5f, 0.0f, 0.0f, 1.0f ),	// Center Point
-		Vertex( 0.5f, -0.5f, 0.0f, 1.0f, 0.0f ),	// Rigth Point
+		Vertex( -0.5f, -0.5f,  1.0f, 1.0f, 0.0f, 0.0f ),	// Bottom Left
+		Vertex( 0.0f,   0.5f,  1.0f, 1.0f, 0.0f, 0.0f ),	// Top Middle
+		Vertex( 0.5f,  -0.5f,  1.0f, 1.0f, 0.0f, 0.0f ),	// Bottom Right
 	};
 
 	D3D11_BUFFER_DESC vertexBufferDesc;
@@ -203,6 +256,33 @@ bool Graphics::InitializeScene()
 	vertexBufferData.pSysMem = v;
 
 	HRESULT hr = this->device->CreateBuffer( &vertexBufferDesc, &vertexBufferData, this->vertexBuffer.GetAddressOf() );
+	if ( FAILED( hr ) )
+	{
+		ErrorLogger::Log( hr, "Failed to create vertex buffer." );
+
+		return false;
+	}
+
+	// Triangle 2 (Green)
+	Vertex v2[] =
+	{
+		Vertex( -0.25f, -0.25f, 0.0f, 0.0f, 1.0f, 0.0f ),	// Bottom Left
+		Vertex( 0.0f,    0.25f, 0.0f, 0.0f, 1.0f, 0.0f ),	// Top Middle
+		Vertex( 0.25f,  -0.25f, 0.0f, 0.0f, 1.0f, 0.0f ),	// Bottom Right
+	};
+
+	ZeroMemory( &vertexBufferDesc, sizeof( vertexBufferDesc ) );
+
+	vertexBufferDesc.Usage = D3D11_USAGE_DEFAULT;
+	vertexBufferDesc.ByteWidth = sizeof( Vertex ) * ARRAYSIZE( v2 );
+	vertexBufferDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	vertexBufferDesc.CPUAccessFlags = 0;
+	vertexBufferDesc.MiscFlags = 0;
+
+	ZeroMemory( &vertexBufferData, sizeof( vertexBufferData ) );
+	vertexBufferData.pSysMem = v2;
+
+	hr = this->device->CreateBuffer( &vertexBufferDesc, &vertexBufferData, this->vertexBuffer2.GetAddressOf() );
 	if ( FAILED( hr ) )
 	{
 		ErrorLogger::Log( hr, "Failed to create vertex buffer." );
