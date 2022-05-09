937c7f9e7ef214a6df787d35845e8ea19785951f
박재복 <pjb24@daum.net>
2022-05-09 23:02:12 +0900

    텍스처를 사용해 그림 표출.
    ShaderResourceView 생성.
    텍스처의 늘어짐 처리를 위해 SamplerState 사용.
    Vertex/Pixel Shader, InputLayout에 Color 대신 TexCoord 사용.
    
    파일을 표출하기 위해 삼각형 2개를 사용해 사각형을 렌더링.
    파일로부터 텍스처를 만들기위해서 WICTextureLoader 사용.

--- DirectX-11-Engine-Practice/Source.cpp
+++ DirectX-11-Engine-Practice/Source.cpp
@@ -1,10 +1,17 @@
 #include "Engine.h"
 
 int APIENTRY wWinMain( _In_ HINSTANCE hInstance,
-					   _In_opt_ HINSTANCE hPrevInstance,
-					   _In_ LPWSTR lpCmdLine,
-					   _In_ int nCmdShow )
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine,
+	_In_ int nCmdShow )
 {
+	HRESULT hr = CoInitialize( NULL );
+	if ( FAILED( hr ) )
+	{
+		ErrorLogger::Log( hr, "Failed to call CoInitialize." );
+		return -1;
+	}
+
 	Engine engine;
 
 	if ( engine.Initialize( hInstance, "Title", "MyWindowClas", 800, 600 ) )
