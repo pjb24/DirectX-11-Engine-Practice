4aff7100547b788c02e08383daf51c44568fd43e
박재복 <pjb24@daum.net>
2022-07-13 18:10:10 +0900

    Blending과 Cube Culling.

--- DirectX-11-Engine-Practice/Graphics/Graphics.cpp
+++ DirectX-11-Engine-Practice/Graphics/Graphics.cpp
@@ -49,33 +49,8 @@ void Graphics::RenderFrame()
 
 	UINT offset = 0;
 
-	{ //Pavement
-		// Update Constant Buffer
-		static float translationOffset[3] = { 0, 0, 4.0 };
-		XMMATRIX world = XMMatrixScaling( 5.0f, 5.0f, 5.0f ) * XMMatrixTranslation( translationOffset[0], translationOffset[1], translationOffset[2] );
-		cb_vs_vertexshader.data.mat = world * this->camera.GetViewMatrix() * this->camera.GetProjectionMatrix();
-		cb_vs_vertexshader.data.mat = DirectX::XMMatrixTranspose( cb_vs_vertexshader.data.mat );	// 행렬 전치
-
-		if ( !cb_vs_vertexshader.ApplyChanges() )
-		{
-			return;
-		}
-		this->deviceContext->VSSetConstantBuffers( 0, 1, this->cb_vs_vertexshader.GetAddressOf() );
-
-		this->cb_ps_pixelshader.data.alpha = 1.0f;
-		this->cb_ps_pixelshader.ApplyChanges();
-		this->deviceContext->PSSetConstantBuffers( 0, 1, this->cb_ps_pixelshader.GetAddressOf() );
-
-		// Square
-		this->deviceContext->PSSetShaderResources( 0, 1, this->pavementTexture.GetAddressOf() );
-		this->deviceContext->IASetVertexBuffers( 0, 1, vertexBuffer.GetAddressOf(), vertexBuffer.StridePtr(), &offset );
-		this->deviceContext->IASetIndexBuffer( indicesBuffer.Get(), DXGI_FORMAT_R32_UINT, 0 );
-
-		this->deviceContext->DrawIndexed( indicesBuffer.BufferSize(), 0, 0 );
-	}
-
-	static float alpha = 0.1f;
-	{ //Pink Texture
+	static float alpha = 0.5f;
+	{ //Pavement Texture
 		// Update Constant Buffer
 		static float translationOffset[3] = { 0, 0, -1.0f };
 		XMMATRIX world = XMMatrixTranslation( translationOffset[0], translationOffset[1], translationOffset[2] );
@@ -93,35 +68,12 @@ void Graphics::RenderFrame()
 		this->deviceContext->PSSetConstantBuffers( 0, 1, this->cb_ps_pixelshader.GetAddressOf() );
 
 		// Square
-		this->deviceContext->PSSetShaderResources( 0, 1, this->pinkTexture.GetAddressOf() );
+		this->deviceContext->PSSetShaderResources( 0, 1, this->pavementTexture.GetAddressOf() );
 		this->deviceContext->IASetVertexBuffers( 0, 1, vertexBuffer.GetAddressOf(), vertexBuffer.StridePtr(), &offset );
 		this->deviceContext->IASetIndexBuffer( indicesBuffer.Get(), DXGI_FORMAT_R32_UINT, 0 );
-
+		this->deviceContext->RSSetState( this->rasterizerState_CullFront.Get() );
 		this->deviceContext->DrawIndexed( indicesBuffer.BufferSize(), 0, 0 );
-	}
-
-	{ //Grass
-		// Update Constant Buffer
-		static float translationOffset[3] = { 0, 0, 0 };
-		XMMATRIX world = XMMatrixScaling( 5.0f, 5.0f, 5.0f ) * XMMatrixTranslation( translationOffset[0], translationOffset[1], translationOffset[2] );
-		cb_vs_vertexshader.data.mat = world * this->camera.GetViewMatrix() * this->camera.GetProjectionMatrix();
-		cb_vs_vertexshader.data.mat = DirectX::XMMatrixTranspose( cb_vs_vertexshader.data.mat );	// 행렬 전치
-
-		if ( !cb_vs_vertexshader.ApplyChanges() )
-		{
-			return;
-		}
-		this->deviceContext->VSSetConstantBuffers( 0, 1, this->cb_vs_vertexshader.GetAddressOf() );
-
-		this->cb_ps_pixelshader.data.alpha = 1.0f;
-		this->cb_ps_pixelshader.ApplyChanges();
-		this->deviceContext->PSSetConstantBuffers( 0, 1, this->cb_ps_pixelshader.GetAddressOf() );
-
-		// Square
-		this->deviceContext->PSSetShaderResources( 0, 1, this->grassTexture.GetAddressOf() );
-		this->deviceContext->IASetVertexBuffers( 0, 1, vertexBuffer.GetAddressOf(), vertexBuffer.StridePtr(), &offset );
-		this->deviceContext->IASetIndexBuffer( indicesBuffer.Get(), DXGI_FORMAT_R32_UINT, 0 );
-
+		this->deviceContext->RSSetState( this->rasterizerState.Get() );
 		this->deviceContext->DrawIndexed( indicesBuffer.BufferSize(), 0, 0 );
 	}
 
@@ -268,6 +220,7 @@ bool Graphics::InitializeDirectX( HWND hwnd )
 	depthstencildesc.DepthEnable = true;
 	depthstencildesc.DepthWriteMask = D3D11_DEPTH_WRITE_MASK::D3D11_DEPTH_WRITE_MASK_ALL;
 	depthstencildesc.DepthFunc = D3D11_COMPARISON_FUNC::D3D11_COMPARISON_LESS_EQUAL;
+	//depthstencildesc.DepthFunc = D3D11_COMPARISON_FUNC::D3D11_COMPARISON_ALWAYS;
 
 	hr = this->device->CreateDepthStencilState( &depthstencildesc, this->depthStencilState.GetAddressOf() );
 	if ( FAILED( hr ) )
@@ -297,6 +250,19 @@ bool Graphics::InitializeDirectX( HWND hwnd )
 	rasterizerDesc.FillMode = D3D11_FILL_MODE::D3D11_FILL_SOLID;
 	rasterizerDesc.CullMode = D3D11_CULL_MODE::D3D11_CULL_BACK;
 	hr = this->device->CreateRasterizerState( &rasterizerDesc, this->rasterizerState.GetAddressOf() );
+	if ( FAILED( hr ) )
+	{
+		ErrorLogger::Log( hr, "Failed to create rasterizer state." );
+		return false;
+	}
+
+	// Create Rasterizer State for culling front
+	D3D11_RASTERIZER_DESC rasterizerDesc_CullFront;
+	ZeroMemory( &rasterizerDesc_CullFront, sizeof( D3D11_RASTERIZER_DESC ) );
+
+	rasterizerDesc_CullFront.FillMode = D3D11_FILL_MODE::D3D11_FILL_SOLID;
+	rasterizerDesc_CullFront.CullMode = D3D11_CULL_MODE::D3D11_CULL_FRONT;
+	hr = this->device->CreateRasterizerState( &rasterizerDesc_CullFront, this->rasterizerState_CullFront.GetAddressOf() );
 	if ( FAILED( hr ) )
 	{
 		ErrorLogger::Log( hr, "Failed to create rasterizer state." );
@@ -338,7 +304,7 @@ bool Graphics::InitializeDirectX( HWND hwnd )
 	sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;	// X Coordinate
 	sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;	// Y Coordinate
 	sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;	// Z Coordinate
-	sampDesc.ComparisonFunc = D3D11_COMPARISON_NEVER;
+	sampDesc.ComparisonFunc = D3D11_COMPARISON_FUNC::D3D11_COMPARISON_NEVER;
 	sampDesc.MinLOD = 0;
 	sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
 	hr = this->device->CreateSamplerState( &sampDesc, this->samplerState.GetAddressOf() );	// Create sampler state.
@@ -399,10 +365,15 @@ bool Graphics::InitializeScene()
 	// Textured Square
 	Vertex v[] =
 	{
-		Vertex( -0.5f, -0.5f, 0.0f, 0.0f, 1.0f ),	// Bottom Left	- [0]
-		Vertex( -0.5f,  0.5f, 0.0f, 0.0f, 0.0f ),	// Top Left		- [1]
-		Vertex( 0.5f,  0.5f, 0.0f, 1.0f, 0.0f ),	// Top Right	- [2]
-		Vertex( 0.5f,  -0.5f, 0.0f, 1.0f, 1.0f ),	// Bottom Right	- [3]
+		Vertex( -0.5f, -0.5f, -0.5f, 0.0f, 1.0f ),	//FRONT Bottom Left	- [0]
+		Vertex( -0.5f,  0.5f, -0.5f, 0.0f, 0.0f ),	//FRONT Top Left	- [1]
+		Vertex( 0.5f,  0.5f, -0.5f, 1.0f, 0.0f ),	//FRONT Top Right	- [2]
+		Vertex( 0.5f,  -0.5f, -0.5f, 1.0f, 1.0f ),	//FRONT Bottom Right- [3]
+
+		Vertex( -0.5f, -0.5f, 0.5f, 0.0f, 1.0f ),	//BACK Bottom Left	- [4]
+		Vertex( -0.5f,  0.5f, 0.5f, 0.0f, 0.0f ),	//BACK Top Left		- [5]
+		Vertex( 0.5f,  0.5f, 0.5f, 1.0f, 0.0f ),	//BACK Top Right	- [6]
+		Vertex( 0.5f,  -0.5f, 0.5f, 1.0f, 1.0f ),	//BACK Bottom Right	- [7]
 	};
 
 	// Load Vertex Data
@@ -415,8 +386,23 @@ bool Graphics::InitializeScene()
 
 	DWORD indices[] =
 	{
-		0, 1, 2,
-		0, 2, 3
+		0, 1, 2,	//FRONT
+		0, 2, 3,	//FRONT
+
+		4, 7, 6,	//BACK
+		4, 6, 5,	//BACK
+
+		3, 2, 6,	//RIGHT SIDE
+		3, 6, 7,	//RIGHT SIDE
+
+		4, 5, 1,	//LEFT SIDE
+		4, 1, 0,	//LEFT SIDE
+
+		1, 5, 6,	//TOP
+		1, 6, 2,	//TOP
+
+		0, 3, 7,	//BOTTOM
+		0, 7, 4,	//BOTTOM
 	};
 
 	// Load Index Data
